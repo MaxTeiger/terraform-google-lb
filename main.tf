20607df8e587a297c81826c288ca03cc06b6b6cf
spoukke <sacha.bernheim@hey.com>
2022-01-14 15:30:45 +0100

    chore: remove useless comments

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# Here you can reference 2 type of terraform objects :
-# 1. Ressources from you provider of choice
-# 2. Modules from official repositories which include modules from the following github organizations
-#     - AWS: https://github.com/terraform-aws-modules
-#     - GCP: https://github.com/terraform-google-modules
-#     - Azure: https://github.com/Azure
-
 resource "google_compute_global_address" "this" {
   count      = var.ip_address == "" ? 1 : 0
   name       = var.name
