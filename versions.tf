0bc87689c10a7943de220477a6ac1feebc01338d
Nicolas Levée <nicolas.levee@gmail.com>
2022-02-11 16:40:47 +0100

    chore(versions.tf): add version for random provider

--- versions.tf
+++ versions.tf
@@ -4,7 +4,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.9.0"
+      version = "~> 3.9.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
     }
   }
 }
